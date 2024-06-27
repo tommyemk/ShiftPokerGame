4c2c44ab05742bca359b83928cf9eb2200902878
tommyemk <159800790+tommyemk@users.noreply.github.com>
2024-06-27 18:24:26 +0200

    Create AbstractRefreshingService.kt

---
+++ src/main/kotlin/service/AbstractRefreshingService.kt
@@ -0,0 +1,39 @@
+package service
+
+import view.Refreshable
+
+/**
+ * Abstract class that provides a method to call a method on all
+ */
+abstract class AbstractRefreshingService {
+    private val refreshables = mutableListOf<Refreshable>()
+
+    /**
+     * adds a [Refreshable] to the list that gets called
+     * whenever [onAllRefreshables] is used.
+     */
+    fun addRefreshable(newRefreshable : Refreshable) {
+        refreshables += newRefreshable
+    }
+
+    /**
+     * Executes the passed method (usually a lambda) on all
+     * [Refreshable]s registered with the service class that
+     * extends this [AbstractRefreshingService]
+     *
+     * Example usage (from any method within the service):
+     * ```
+     * onAllRefreshables {
+     *   refreshPlayerStack(p1, p1.playStack)
+     *   refreshPlayerStack(p2, p2.playStack)
+     *   refreshPlayerStack(p1, p1.collectedCardsStack)
+     *   refreshPlayerStack(p2, p2.collectedCardsStack)
+     * }
+     * ```
+     *
+     */
+    fun onAllRefreshables(method: Refreshable.() -> Unit) =
+        refreshables.forEach { it.method() }
+
+
+}
